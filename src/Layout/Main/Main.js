e591dd363e274831c369fd3ccae07a6d5cb6b833
mkmahmud <mahmudulhasanmk434@gmail.com>
2022-12-13 23:22:47 +0600

    category section

--- src/Layout/Main/Main.js
+++ src/Layout/Main/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Category from '../../Pages/Shared/Category/Category';
 import Navbar from '../../Pages/Shared/Navbar/Navbar';
 
 const Main = () => {
     return (
         <div>
           <Navbar></Navbar>  
+          <Category></Category>
           <Outlet></Outlet>
         </div>
     );
---
+++ src/Pages/Shared/Category/Category.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+
+const Category = () => {
+
+    // loaded Category Data
+    const [category, setCategory] = useState([])
+
+
+    // Fetch Category Data
+    useEffect(() => {
+        fetch('category.json')
+            .then(res => res.json())
+            .then(data => setCategory(data))
+    }, [])
+
+    console.log(category)
+
+
+    return (
+        <div className='category border-y-2'>
+            <ul className="menu menu-horizontal px-1">
+                {
+                    category.map(cate => <li tabIndex={0}>
+                        <a>
+                            {cate.categoryName}
+                            {
+                                cate?.subcategory ? <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg> : ''
+                            }
+                            
+                        </a>
+                        <ul className="p-2 bg-base-100">
+                            
+                            {
+                                cate?.subcategory?.map(sub => <li><a>{sub.name}</a></li>)
+                            }
+                        </ul>
+                    </li>)
+                }
+
+            </ul>
+        </div>
+    );
+};
+
+export default Category;
